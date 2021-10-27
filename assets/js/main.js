6f3a640a294cf6c1f65e71940a66d2fbf637d3e1
Wendler <wendlermoa@gmail.com>
2021-10-27 12:20:07 -0300

    Updated main.js from JS to jQuery (just for tests)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,66 @@
-'use strict';
+$(document).ready(function () {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#endereço").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#estado").val("");
+    }
+
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function () {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if (validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#endereço").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#estado").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/" + cep + "/json/?callback=?", function (dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#endereço").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#estado").val(dados.uf);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado.");
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
+
+/*'use strict';
+
 
 const limparFormulario = (endereco) =>{
     document.getElementById('endereço').value = '';
@@ -17,11 +79,12 @@ const preencherFormulario = (endereço) => {
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 
 const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
-
 /* Digitos totais ou caso tenha letra (eNumero) de um CEP, caso contrario retorna error*/
 
-const pesquisarCep = async () => {
+/*const pesquisarCep = async () => {
     limparFormulario();
+
+
 
     const cep = document.getElementById('cep').value;
     const url = `https://viacep.com.br/ws/${cep}/json/`;
@@ -29,7 +92,7 @@ const pesquisarCep = async () => {
         const dados = await fetch(url);
         const endereço = await dados.json();
         /* Vou pegar os dados da promeça fetch, armazena os dados no json e ja pega todo o cep digitado*/
-        if (endereço.hasOwnProperty('erro')) {
+/*        if (endereço.hasOwnProperty('erro')) {
             document.getElementById('endereço').value = 'CEP não encontrado';
         }
         else {
@@ -42,4 +105,10 @@ const pesquisarCep = async () => {
 }
 
 document.getElementById('cep')
-    .addEventListener('focusout', pesquisarCep);
+    .addEventListener('focusout', pesquisarCep);*/
+
+
+
+
+
+
